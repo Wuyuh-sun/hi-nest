55600f57770bde5843fc709604f518661810aabd
yunha <wjh0970@gmail.com>
2022-12-13 22:21:04 +0900

    nest_ing...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //인풋 결과의 유효성 검사 하는 미들웨어
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(8000);
 }
 bootstrap();
