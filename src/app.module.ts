18546d11e50880bd1ad4c5d4399613415e72fda5
wooyunha <wjh0970@gmail.com>
2023-08-07 00:03:36 +0900

    dependency injection : 의존성 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+// import { MoviesController } from './movies/movies.controller';
+// import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  // controllers: [MoviesController],
+  // providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -11,4 +11,6 @@ import { CreateMovieDto } from './create-movie.dto';
 //   @IsString({ each: true })
 //   readonly generes?: string[];
 // }
+
+// PartialType으로 매핑할 경우 파라미터로 받는 DTO 타입과 동일하되 옵셔널해짐
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
