ac24752bf5a63b4ec8966ecc00f53189cdf116fd
wooyunha <wjh0970@gmail.com>
2023-08-06 23:23:33 +0900

    인풋 결과 유효성 검사 미들웨어

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ async function bootstrap() {
   //인풋 결과의 유효성 검사 하는 미들웨어
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
+      whitelist: true, // whitelist:true 일 경우 decorator가 없는 property는 거른다
+      forbidNonWhitelisted: true, // forbidNonWhitelisted -> 엔티티 데코레이터에 없는 값 인입시 그 값에 대한 에러메세지 알려줌
+      transform: true, // transform -> 컨트롤러가 값을 받을때 컨트롤러에 정의한 타입으로 형변환
     }),
   );
   await app.listen(8000);
--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -5,6 +5,7 @@ export class CreateMovieDto {
   readonly title: string;
   @IsNumber()
   readonly year: number;
+  // 배열이면 each true, 옵션값이면 IsOptional 사용
   @IsString({ each: true })
   readonly generes: string[];
 }
